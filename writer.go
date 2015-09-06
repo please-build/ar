c2ad992593088d7dfc8b636990f3cf34bd292b32
Advisory Lock <advisory.lock@gmail.com>
2015-09-06 08:16:05 -0700

    Fix the writer to make io.Copy() work correctly.

--- writer.go
+++ writer.go
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright (c) 2013 Blake Smith <blakesmith0@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,7 +28,7 @@ import (
 )
 
 var (
-	ErrWriteTooLong    = errors.New("ar: write too long")
+	ErrWriteTooLong = errors.New("ar: write too long")
 )
 
 // Writer provides sequential writing of an ar archive.
@@ -45,7 +45,7 @@ var (
 // }
 // io.Copy(archive, data)
 type Writer struct {
-	w io.Writer
+	w  io.Writer
 	nb int64 // number of unwritten bytes for the current file entry
 }
 
@@ -91,8 +91,11 @@ func (aw *Writer) Write(b []byte) (n int, err error) {
 	}
 
 	if len(b)%2 == 1 { // data size must be aligned to an even byte
-		n2, _ := aw.w.Write([]byte{'\n'})
-		return n+n2, err
+		if _, err := aw.w.Write([]byte{'\n'}); err != nil {
+			// Return n although we actually wrote n+1 bytes.
+			// This is to make io.Copy() to work correctly.
+			return n, err
+		}
 	}
 
 	return
